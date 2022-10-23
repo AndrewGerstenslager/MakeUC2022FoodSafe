babe20a9d57e100a75b3838dbdcfce96b99c5de2
ThomasT321 <99148041+ThomasT321@users.noreply.github.com>
2022-10-23 02:27:54 -0400

    Add files via upload

---
+++ HackathonXAMLApp/Classes/User.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackathonXAMLApp
+{
+    public class User : INotifyPropertyChanged
+    {
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		#region Properties
+		private string _barcode;
+		public string Barcode
+		{
+			get
+			{
+				return _barcode;
+			}
+			set
+			{
+				_barcode = value;
+				OnPropertyChanged(nameof(Barcode));
+			}
+		}
+		private string _name;
+		public string Name
+		{
+			get
+			{
+				return _name;
+			}
+			set
+			{
+                _name = value;
+				OnPropertyChanged(nameof(Name));
+			}
+		}
+		private string _allergies;
+		public string Allergies
+		{
+			get
+			{
+				return _allergies;
+			}
+			set
+			{
+				_allergies = value;
+				OnPropertyChanged(nameof(Allergies));
+			}
+		}
+		private bool _vegan;
+		public bool Vegan
+		{
+			get
+			{
+				return _vegan;
+			}
+			set
+			{
+				_vegan = value;
+				OnPropertyChanged(nameof(Vegan));
+			}
+		}
+		private bool _vegetarian;
+		public bool Vegetarian
+		{
+			get
+			{
+				return _vegetarian;
+			}
+			set
+			{
+				_vegetarian = value;
+				OnPropertyChanged(nameof(Vegetarian));
+			}
+		}
+		private bool _glutenFree;
+		public bool GlutenFree
+		{
+			get
+			{
+				return _glutenFree;
+			}
+			set
+			{
+				_glutenFree = value;
+				OnPropertyChanged(nameof(GlutenFree));
+			}
+		}
+		private bool _dairyFree;
+		public bool DairyFree
+		{
+			get
+			{
+				return _dairyFree;
+			}
+			set
+			{
+				_dairyFree = value;
+				OnPropertyChanged(nameof(DairyFree));
+			}
+		}
+		private bool _crohns;
+		public bool Crohns
+		{
+			get
+			{
+				return _crohns;
+			}
+			set
+			{
+				_crohns = value;
+				OnPropertyChanged(nameof(Crohns));
+			}
+		}
+		private bool _keto;
+		public bool Keto
+		{
+			get
+			{
+				return _keto;
+			}
+			set
+			{
+				_keto = value;
+				OnPropertyChanged(nameof(Keto));
+			}
+		}
+		#endregion
+		protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
---
+++ HackathonXAMLApp/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HackathonXAMLApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateProfileButtonClick(object sender, RoutedEventArgs e)
+        {
+            var window = new CreateProfile();
+            window.Show();
+        }
+        private void CameraButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
