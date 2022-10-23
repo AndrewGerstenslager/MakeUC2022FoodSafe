68c0c77e19e9a5990169594ba2ea5b88e8b47c3a
Wissy04 <114517029+Wissy04@users.noreply.github.com>
2022-10-23 10:35:22 -0400

    Add files via upload

---
+++ MakeUC2022FoodSafeCompiled/Converters/FoodXToVisibilityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HackathonXAMLApp.Converters
+{
+    public class FoodXToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Visibility visibility = Visibility.Hidden;
+            if (value.Equals(false))
+            {
+                visibility = Visibility.Hidden;
+            }
+            else
+            {
+                visibility = Visibility.Visible;
+            }
+            return visibility;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
